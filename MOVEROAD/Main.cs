6c7c0231111c808cfce742b2e772d4ad9e140251
5359998@stu.kmu.ac.kr <5359998@stu.kmu.ac.kr>
2020-11-02 22:58:20 +0900

    v0.5

--- MOVEROAD/Main.cs
+++ MOVEROAD/Main.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,10 +14,17 @@ namespace MOVEROAD
     public partial class Main : Form
     {
         private Point mousePoint;
-
+ 
         public Main()
         {
             InitializeComponent();
+            //DBConnetion.getInstance().getDBConnetion();
+            new Thread(delegate ()
+            {
+                DBConnetion.getInstance().InitSocket();
+            }).Start();
+
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
