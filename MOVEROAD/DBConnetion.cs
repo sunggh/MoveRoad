3df6c156e13997dc4c65e0ce9a29fcc403e42bcf
5359998@stu.kmu.ac.kr <5359998@stu.kmu.ac.kr>
2020-11-02 22:58:20 +0900

    v0.5

--- MOVEROAD/DBConnetion.cs
+++ MOVEROAD/DBConnetion.cs
@@ -2,18 +2,21 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace MOVEROAD
 {
     public class DBConnetion
     {
-        private string DBIp = "localhost";
+        private string DBIp = "3.138.172.1";
         private string DBId = "root";
         private string DBPass = "root";
-        private string DBName = "project";
+        private string DBName = "dbproject";
         private static DBConnetion instance_ = new DBConnetion();
+        TcpClient clientSocket;
         public static DBConnetion getInstance()
         {
             return instance_;
@@ -34,5 +37,22 @@ namespace MOVEROAD
                 return null;
             }
         }
+        public void InitSocket()
+        {
+            try
+            {
+                clientSocket.Connect(DBIp, 9999);
+                MessageBox.Show("서버 연결 완료");
+            }
+            catch (SocketException se)
+            {
+             
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+
     }
 }
