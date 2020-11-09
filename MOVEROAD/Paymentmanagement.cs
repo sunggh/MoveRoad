967dfbc964e13f53dd086dd574ef9e9528fc7c44
동길 이 <oio3215@naver.com>
2020-11-09 22:59:46 +0900

    일단 저장하고 빠짐 LDG

---
+++ MOVEROAD/Paymentmanagement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MOVEROAD
+{
+    public partial class Paymentmanagement : Form
+    {
+        private static Paymentmanagement instance_ = new Paymentmanagement();
+
+        public static Paymentmanagement getinstance()
+        {
+            return instance_;
+        }
+        public Paymentmanagement()
+        {
+            InitializeComponent();
+        }
+    }
+}
