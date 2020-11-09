316ddf6021a81f348b538fefb619ce5908de9aa2
seojeong <5532866@stu.kmu.ac.kr>
2020-11-09 23:51:16 +0900

    [LSJ] Task 화면 구성 (1)

---
+++ MOVEROAD/TaskForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿using System.Drawing;
+
+namespace MOVEROAD
+{
+    partial class TaskForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TaskForm));
+            this.buttonRegistration = new System.Windows.Forms.Button();
+            this.buttonManagement = new System.Windows.Forms.Button();
+            this.buttonMasterManagement = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonRegistration
+            // 
+            this.buttonRegistration.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(71)))), ((int)(((byte)(117)))));
+            this.buttonRegistration.ForeColor = System.Drawing.Color.White;
+            this.buttonRegistration.Location = new System.Drawing.Point(48, 97);
+            this.buttonRegistration.Name = "buttonRegistration";
+            this.buttonRegistration.Size = new System.Drawing.Size(154, 36);
+            this.buttonRegistration.TabIndex = 0;
+            this.buttonRegistration.Text = "일일 업무등록";
+            this.buttonRegistration.UseVisualStyleBackColor = false;
+            // 
+            // buttonManagement
+            // 
+            this.buttonManagement.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(71)))), ((int)(((byte)(117)))));
+            this.buttonManagement.ForeColor = System.Drawing.Color.White;
+            this.buttonManagement.Location = new System.Drawing.Point(48, 182);
+            this.buttonManagement.Name = "buttonManagement";
+            this.buttonManagement.Size = new System.Drawing.Size(154, 36);
+            this.buttonManagement.TabIndex = 1;
+            this.buttonManagement.Text = "일일 업무관리";
+            this.buttonManagement.UseVisualStyleBackColor = false;
+            // 
+            // buttonMasterManagement
+            // 
+            this.buttonMasterManagement.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(71)))), ((int)(((byte)(117)))));
+            this.buttonMasterManagement.ForeColor = System.Drawing.Color.White;
+            this.buttonMasterManagement.Location = new System.Drawing.Point(48, 268);
+            this.buttonMasterManagement.Name = "buttonMasterManagement";
+            this.buttonMasterManagement.Size = new System.Drawing.Size(154, 36);
+            this.buttonMasterManagement.TabIndex = 2;
+            this.buttonMasterManagement.Text = "업무 마스터 관리";
+            this.buttonMasterManagement.UseVisualStyleBackColor = false;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(71)))), ((int)(((byte)(117)))));
+            this.buttonSearch.ForeColor = System.Drawing.Color.White;
+            this.buttonSearch.Location = new System.Drawing.Point(308, 22);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(154, 36);
+            this.buttonSearch.TabIndex = 3;
+            this.buttonSearch.Text = "일일 업무 검색";
+            this.buttonSearch.UseVisualStyleBackColor = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(308, 89);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(314, 159);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(7, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TaskForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(745, 412);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonMasterManagement);
+            this.Controls.Add(this.buttonManagement);
+            this.Controls.Add(this.buttonRegistration);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "TaskForm";
+            this.Text = "TaskForm";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRegistration;
+        private System.Windows.Forms.Button buttonManagement;
+        private System.Windows.Forms.Button buttonMasterManagement;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
