7e84f85fd43a36a6b643e262a4a725e52c41a980
동길 이 <oio3215@naver.com>
2020-11-11 01:57:13 +0900

    123

---
+++ MOVEROAD/depart_add_event.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MOVEROAD
+{
+    public partial class depart_add_event : Form
+    {
+        public depart_add_event()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_cancel_Click(object sender, EventArgs e)
+        {
+            Application.OpenForms["depart_add_event"].Close();
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            string depart_name = tb_depart_name.Text;
+            string depart_
+            string add_query = "insert into department(name,manager) values ()"
+            DBConnetion
+        }
+    }
+}
