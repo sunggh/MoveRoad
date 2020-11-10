fd2133d1dc4171167c51477d37e8bf55fef39ecc
동길 이 <oio3215@naver.com>
2020-11-11 01:57:13 +0900

    123

--- MOVEROAD/Departmanagement.cs
+++ MOVEROAD/Departmanagement.cs
@@ -28,6 +28,15 @@ namespace MOVEROAD
         public void departGDV()
         {
             //user 테이블 이용해서 그리드뷰 만들기
+            //cell 클릭 이벤트 이용해서 부서에 대한 정보 표기하기
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            //새 폼 띄우고 ShowDialog로 다른행동 못하게 금지
+            //
+            depart_add_event add = new depart_add_event();
+            add.ShowDialog();
         }
     }
 }
