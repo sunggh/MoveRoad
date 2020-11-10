b1fee1a51337879d3d55388d996ba41746841d66
5359998@stu.kmu.ac.kr <5359998@stu.kmu.ac.kr>
2020-11-11 01:58:16 +0900

    .

--- MOVEROAD/Program.cs
+++ MOVEROAD/Program.cs
@@ -17,7 +17,7 @@ namespace MOVEROAD
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            Application.Run(new test());
         }
     }
 }
