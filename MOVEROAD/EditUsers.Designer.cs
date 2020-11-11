c8934e02f8137c781ed04a15bd7031b45d454a91
상 김 <kkho3@nate.com>
2020-11-11 17:57:40 +0900

    UI 사이즈 변경완료, AddUsers 쿼리 오류 수정해야함

--- MOVEROAD/EditUsers.Designer.cs
+++ MOVEROAD/EditUsers.Designer.cs
@@ -37,18 +37,18 @@
             // 
             // buttonEditUsers
             // 
-            this.buttonEditUsers.Location = new System.Drawing.Point(534, 12);
+            this.buttonEditUsers.Location = new System.Drawing.Point(498, 12);
             this.buttonEditUsers.Name = "buttonEditUsers";
-            this.buttonEditUsers.Size = new System.Drawing.Size(255, 51);
+            this.buttonEditUsers.Size = new System.Drawing.Size(235, 51);
             this.buttonEditUsers.TabIndex = 7;
             this.buttonEditUsers.Text = "사원 정보 수정";
             this.buttonEditUsers.UseVisualStyleBackColor = true;
             // 
             // buttonSearchUsers
             // 
-            this.buttonSearchUsers.Location = new System.Drawing.Point(273, 12);
+            this.buttonSearchUsers.Location = new System.Drawing.Point(253, 12);
             this.buttonSearchUsers.Name = "buttonSearchUsers";
-            this.buttonSearchUsers.Size = new System.Drawing.Size(255, 51);
+            this.buttonSearchUsers.Size = new System.Drawing.Size(235, 51);
             this.buttonSearchUsers.TabIndex = 6;
             this.buttonSearchUsers.Text = "사원 검색";
             this.buttonSearchUsers.UseVisualStyleBackColor = true;
@@ -57,7 +57,7 @@
             // 
             this.buttonAddUsers.Location = new System.Drawing.Point(12, 12);
             this.buttonAddUsers.Name = "buttonAddUsers";
-            this.buttonAddUsers.Size = new System.Drawing.Size(255, 51);
+            this.buttonAddUsers.Size = new System.Drawing.Size(235, 51);
             this.buttonAddUsers.TabIndex = 5;
             this.buttonAddUsers.Text = "사원 등록";
             this.buttonAddUsers.UseVisualStyleBackColor = true;
@@ -68,18 +68,19 @@
             this.dataGridView1.Location = new System.Drawing.Point(12, 69);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(776, 369);
+            this.dataGridView1.Size = new System.Drawing.Size(721, 331);
             this.dataGridView1.TabIndex = 9;
             // 
             // EditUsers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(745, 412);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.buttonEditUsers);
             this.Controls.Add(this.buttonSearchUsers);
             this.Controls.Add(this.buttonAddUsers);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "EditUsers";
             this.Text = "EditUsers";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
