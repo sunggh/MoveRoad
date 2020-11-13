519363961cfc0f0b28b7c04c90cc485a185cbe2a
sagongjieun <gonii0821@naver.com>
2020-11-13 21:08:05 +0900

    SGJE 결재 등록 구현 완성

--- MOVEROAD/SignForm.cs
+++ MOVEROAD/SignForm.cs
@@ -23,12 +23,18 @@ namespace MOVEROAD
             this.main = main;
 
             //업무선택 콤보박스
-            string sql_work = "SELECT name FROM department;";
             foreach (DepartmentInfo department in main.departments)
             {
                 comboBoxWork.Items.Add(department.name);
             }
 
+        }
+        private void comboBoxWork_SelectedIndexChanged(object sender, EventArgs e) //콤보박스의 업무를 선택하면
+        {
+            comboBoxDrafter.Items.Clear(); //초기화
+            string sql_drafter = "SELECT `name` FROM `project`.`user` WHERE `index` = " + main.departments[comboBoxWork.SelectedIndex].manager + "";
+            string drafter = (string)DBConnetion.getInstance().Select(sql_drafter, 3);
+            comboBoxDrafter.Items.Add(drafter);
         }
 
         //InfoFile.DepartmentInfo depart;
@@ -36,24 +42,17 @@ namespace MOVEROAD
         private void buttonInsert_Click(object sender, EventArgs e)
         {
             string title = textBoxTitle.Text;
-            string work = comboBoxWork.Text;
+            //string work = comboBoxWork.Text;
             string drafter = comboBoxDrafter.Text;
             string content = textBoxContent.Text;
             string comment = textBoxComment.Text;
 
-            //결재 전이 아니라 progress를 int로
-            //string query = "INSERT INTO sign(sub_class, comment, drafter, progress) VALUES(1, '" + comment + "', 1, 1)";
-            //DBConnetion.getInstance().Insert(query);
+            //progress = 진행사항 (0:결재 전, 1:결재 중, 2:결재 완료, 3:반려) 등록할때는 항상 progress = 0
+            string query = "INSERT INTO sign(title, text, comment, sub_class, drafter, drafter_to, progress) " +
+                "VALUES('" + title + "', '" + content + "', '" + comment + "', '" + main.departments[comboBoxWork.SelectedIndex].id + "', '" + main.me.index + "', '" + drafter + "', 0)";
+            DBConnetion.getInstance().Insert(query);
 
             MessageBox.Show("결재가 등록되었습니다.", "확인");
-        }
-
-        private void comboBoxWork_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            comboBoxDrafter.Items.Clear();
-            string sql_drafter = "SELECT `name` FROM `project`.`user` WHERE `index` = " + main.departments[comboBoxWork.SelectedIndex].manager + "";
-            string drafter = (string)DBConnetion.getInstance().Select(sql_drafter, 3);
-            comboBoxDrafter.Items.Add(drafter);
         }
     }
 }
