3642a729adacdd0f70fa766a51695dcf1aa24a24
상 김 <kkho3@nate.com>
2020-11-13 15:31:40 +0900

    해야될거 존나 많음

--- MOVEROAD/AddUsers.Designer.cs
+++ MOVEROAD/AddUsers.Designer.cs
@@ -94,6 +94,7 @@
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
             this.dataGridView1.Size = new System.Drawing.Size(721, 202);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
             // 
             // groupBox1
             // 
--- MOVEROAD/SearchUsers.cs
+++ MOVEROAD/SearchUsers.cs
@@ -18,9 +18,30 @@ namespace MOVEROAD
             return instance;
         }
 
+        public int depart;
+        public string name;
+        public int age;
+
         public SearchUsers()
         {
             InitializeComponent();
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            this.depart = comboBoxDepart.SelectedIndex;
+            this.name = textBoxName.Text;
+            this.age = Convert.ToInt32(textBoxAge.Text);
+
+            DataShow_Search();
+        }
+
+        private void DataShow_Search()
+        {
+            string query = "SELECT `index` AS `No.`, depart AS 부서명, grade AS 직위, name AS 이름, age AS 나이, gender AS 성별, phone AS `H.P`, address AS 주소 FROM `user`" +
+                "WHERE `depart` = '" + depart + "' AND `name` = '" + name + "' AND `age` = '" + age + "'";
+            DataTable table = DBConnetion.getInstance().getDBTable(query);
+            dataGridView1.DataSource = table;
         }
         // 부서명, 성명, 나이 입력받아서 검색버튼 누르면 데이터 그리드뷰로 찾아주는 클래스
         // SELECT FROM WHERE 쓰믄댐
