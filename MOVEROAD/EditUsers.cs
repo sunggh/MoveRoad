ced2a6498059b4f64913471bfed2f437b69e5c8e
상 김 <kkho3@nate.com>
2020-11-13 15:31:40 +0900

    해야될거 존나 많음

--- MOVEROAD/EditUsers.cs
+++ MOVEROAD/EditUsers.cs
@@ -21,6 +21,20 @@ namespace MOVEROAD
         public EditUsers()
         {
             InitializeComponent();
+            DataShow();
+        }
+
+        private void DataShow()
+        {
+            string query = "SELECT `index` AS `No.`, depart AS 부서명, grade AS 직위, name AS 이름, age AS 나이, gender AS 성별, phone AS `H.P`, address AS 주소 FROM `user`";
+            DataTable table = DBConnetion.getInstance().getDBTable(query);
+            
+            dataGridView1.DataSource = table;
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
         // 데이터 그리드뷰로 첨부터 사원목록 다 보여주고 한놈 클릭하면 팝업창으로 금마 정보 수정할수있게끔 만들믄댐
     }
