85730409d4d678205dd9e5c7e2c49492ea318801
kanghee <tgc203@naver.com>
2020-11-21 15:10:13 +0900

    출근부 예외

--- MOVEROAD/attendance_card.cs
+++ MOVEROAD/attendance_card.cs
@@ -9,6 +9,11 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 
+// 출근부 구현 위 , 아래 버튼을 누르면 한달 추가, 감소 기능
+// 출근버튼을 누르면 현재 시간을 기점으로 출근됨 = 출근버튼은 한번만 가능
+// 퇴근버튼을 누르면 현재 시간을 기점으로 퇴근됨 = 출근버튼을 누르기 전에 누르면 오류발생 MessageBox로 출력
+// 조회버튼을 통해 월을 기준으로 출퇴근 시간, 일한시간 조회가능
+
 namespace MOVEROAD
 {
     public partial class attendance_card : Form
@@ -23,44 +28,46 @@ namespace MOVEROAD
             this.main = main;
            
         }
-
-
-        private void button1_Click(object sender, EventArgs e) // 출근
+        private void buttonStart_Click(object sender, EventArgs e) // 출근버튼
         {
-            string a = main.me.id;  //현재접속중인 id값
-            object b = DBConnetion.getInstance().Select("SELECT id FROM user WHERE id='" + a + "'", 4);  // 현재 로그인 중인 id값 읽어오기
+            string ID = main.me.id;  //현재접속중인 id값
+            object start = DBConnetion.getInstance().Select("SELECT startTime FROM attendance_card " +
+                "WHERE id='" + ID + "' and date ='" + DateTime.Now.ToString("yyyy-MM-dd") + "'", 5);  // 출근버튼을 클릭하였는지 확인
 
-           
-            DBConnetion.getInstance().Insert("INSERT INTO attendance_card (id, date, startTime)" +
-                    "VALUES('" + a + "','" + DateTime.Now.ToString("yyyy-MM-dd") + "','" + DateTime.Now.ToString("HH:mm") + "')");
+            if ((string)start == null)   // 출근버튼을 누르지 않았더라면
+            {
+                DBConnetion.getInstance().Insert("INSERT INTO attendance_card (id, date, startTime)" +
+                        "VALUES('" + ID + "','" + DateTime.Now.ToString("yyyy-MM-dd") + "','" + DateTime.Now.ToString("HH:mm") + "')");
 
-            MessageBox.Show("현재시각" + DateTime.Now.ToString("HH시 mm분") + "출근 완료");
-          
-            
+                MessageBox.Show("현재시각" + DateTime.Now.ToString("HH시 mm분") + "출근 완료");
+            }
+            else // 출근버튼을 눌렀다면
+                MessageBox.Show("이미 출근 하였습니다.");
         }
 
-        private void button2_Click(object sender, EventArgs e) // 퇴근
+        private void buttonFinish_Click(object sender, EventArgs e) //퇴근 버튼
         {
-            string a = main.me.id;
-            object b = DBConnetion.getInstance().Select("SELECT id FROM attendance_card WHERE id='" + a + "' and startTime != 'null' ", 4); // 현재 id값이 출근을 눌렀는지 확인하기 위한 변수
+            string ID = main.me.id;
+            object start = DBConnetion.getInstance().Select("SELECT startTime FROM attendance_card " +
+                "WHERE id='" + ID + "' and date='" + DateTime.Now.ToString("yyyy-MM-dd") + "' ", 5); // 현재 id값이 출근을 눌렀는지 확인하기 위한 변수
             DateTime dt;
             dt = Convert.ToDateTime(Today.Text);
             string today = dt.ToString("yyyy-MM-dd");
 
-            if ((string)b != null)
+
+
+            if ((string)start != null)
             {// 만약 출근을 눌렀다면 정상적으로 종료시간 업데이트 종료시간 업데이트시 당일날만 업데이트 하기위해 like문으로 date의 값을 당일날이라는 조건으로 걸어둔다
                 DBConnetion.getInstance().Update("UPDATE attendance_card SET finishTime ='" + DateTime.Now.ToString("HH:mm") + "' " +
-                    "WHERE id='" + a + "' and startTime != 'null' and  date like '%" + today + "%'");
+                    "WHERE id='" + ID + "' and startTime != 'null' and  finishTime = '\"' and date like '%" + today + "%'");
 
                 MessageBox.Show("현재시각" + DateTime.Now.ToString("HH:mm") + "퇴근 완료");
                 workTime(); // 퇴근과 동시에 업무시간 업데이트
             }
             else // 만약 출근버튼을 먼저 누르지 않았다면
                 MessageBox.Show("먼저 출근버튼을 눌러주십시오.");
-
-            
         }
-        
+
         private void workTime() // 업무시간 업데이트 (시작시간과 종료시간값을 받아와 DateTime형식으로 변환후 TimeSpan으로 두값을 빼준후 시간을 얻어옴 얻어온 시간값을 Int 형식으로 Convert후 DB에 저장)
         {
             DateTime dt;
@@ -76,7 +83,11 @@ namespace MOVEROAD
             DateTime dt3 = Convert.ToDateTime(finish);
 
             TimeSpan ts;
-            ts = dt3 - dt2;
+            if (dt3 > dt2)
+                ts = dt3 - dt2;
+            else
+                ts = dt2 - dt3;
+
             string hours= ts.ToString("hh");
             int worktime = Convert.ToInt32(hours);  
           
@@ -87,37 +98,32 @@ namespace MOVEROAD
 
         }
 
-        private void button3_Click(object sender, EventArgs e) // 한달추가
+        private void buttonInc_Click(object sender, EventArgs e) // 한달 추가
         {
             DateTime dt;
-            dt = Convert.ToDateTime(label2.Text);      
+            dt = Convert.ToDateTime(label2.Text);
             label2.Text = dt.AddMonths(1).ToString("yyyy-MM");
-
         }
 
-        private void button4_Click(object sender, EventArgs e) //한달감소
+        private void buttonDec_Click(object sender, EventArgs e) // 한달 감소
         {
             DateTime dt;
             dt = Convert.ToDateTime(label2.Text);
             label2.Text = dt.AddMonths(-1).ToString("yyyy-MM");
         }
 
-        private void button5_Click(object sender, EventArgs e) //출근부 조회
+        private void buttonRead_Click(object sender, EventArgs e) // 출근부 조회
         {
             DateTime dt;
             dt = Convert.ToDateTime(label2.Text);
             string a = dt.ToString("yyyy-MM");
-            
-
 
             // 출근부를 조회 현재 년-월을 like문으로 설정하여 년-월 별로 출근부를 조회 할수 있음 
             DataTable tb = DBConnetion.getInstance().getDBTable("SELECT DATE_FORMAT(date, '%d') AS 일, name AS 사용자이름,startTime AS 출근시간 ,finishTime AS 퇴근시간, workTime AS 근무시간"
-                       + " FROM attendance_card join user on attendance_card.id = user.id Where user.id='" + main.me.id + "' and date like '" + a + "%' " );
+                       + " FROM attendance_card join user on attendance_card.id = user.id Where user.id='" + main.me.id + "' and date like '" + a + "%' ");
             dataGridView1.DataSource = tb;
-
-            
         }
 
-       
+        
     }
 }
