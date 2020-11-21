139aaf9d7d2699204d0147acd1a3568ed84cdf72
상 김 <kkho3@nate.com>
2020-11-21 13:07:52 +0900

    추가수당 틀만 잡아둠

--- MOVEROAD/PaymentForm.cs
+++ MOVEROAD/PaymentForm.cs
@@ -12,9 +12,27 @@ namespace MOVEROAD
 {
     public partial class PaymentForm: Form
     {
-        public PaymentForm()
+        MainForm main;
+        
+        int overtime; // 연장근무
+        int nightOvertime; // 야간근무
+        int holidayWork; // 휴일근무
+
+        public PaymentForm(MainForm main)
         {
             InitializeComponent();
+            this.main = main;
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            int select = comboBoxSelect.SelectedIndex;
+            
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
         }
     }
 }
