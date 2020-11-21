fedcfa64b9b7f11642025a5bd2e9d0b6f8797943
이동길 <oio3215@naver.com>
2020-11-21 11:38:04 +0900

    변경1

--- MOVEROAD/PaymentForm.Designer.cs
+++ MOVEROAD/PaymentForm.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // PaymentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(745, 412);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "PaymentForm";
             this.Text = "PaymentForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
