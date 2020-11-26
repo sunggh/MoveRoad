fb9134dc746d1e23156872f641b37f2860cb0be9
상 김 <kkho3@nate.com>
2020-11-27 00:15:51 +0900

    퇴근오류

--- MOVEROAD/PaymentForm.cs
+++ MOVEROAD/PaymentForm.cs
@@ -23,60 +23,44 @@ namespace MOVEROAD
         private void buttonAdd_Click(object sender, EventArgs e)
         {
             string query = "";
-
+            string ID = main.me.id;
             string date = dateTimePicker1.Value.ToShortDateString();
             int select = comboBoxSelect.SelectedIndex;
             int time = Convert.ToInt32(textBoxTime.Text);
             int userIndex = Convert.ToInt32(main.me.index);
 
-            query = "SELECT salary_overtime.date FROM project.salary_overtime WHERE salary_overtime.userIndex = " + userIndex;
+            // 로그인한 사용자가 출근기록이 있는지 확인
+            query = "SELECT salary.date FROM project.salary " +
+                "WHERE salary.index = '" + ID + "' AND " + date;
             object check = DBConnetion.getInstance().Select(query, 8);
 
-            if (check.Equals(0)) // 초과근무 기록이 존재하지 않을 때
+            if (check.Equals(1)) // 출근한 기록이 있을때
             {
                 switch (select)
                 {
                     case 0: // 연장근무
-                        query = "INSERT INTO `salary_overtime` (`userIndex`, `overtime`, `date`)" +
-                            "VALUES (" + userIndex + ", " + time + ", " + date + ")";
+                        query = "UPDATE salary SET salary.overtimePay = '" + time * 15000 + "' " +
+                            "WHERE salary.index = " + ID + " AND salary.date = '" + date + "'";
                         DBConnetion.getInstance().Update(query);
                         MessageBox.Show("연장근무 ( " + time + " ) 시간 등록이 완료되었습니다.", "신청 확인");
                         break;
                     case 1: // 야간근무
-                        query = "INSERT INTO `salary_overtime` (`userIndex`, `nighttime`, `date`)" +
-                            "VALUES (" + userIndex + ", " + time + ", " + date + ")";
+                        query = "UPDATE salary SET salary.nighttimePay = '" + time * 15000 + "' " +
+                            "WHERE salary.index = " + ID + " AND salary.date = '" + date + "'";
                         DBConnetion.getInstance().Update(query);
                         MessageBox.Show("야간근무 ( " + time + " ) 시간 등록이 완료되었습니다.", "신청 확인");
                         break;
                     case 2: // 휴일근무
-                        query = "INSERT INTO `salary_overtime` (`userIndex`, `holidaytime`, `date`)" +
-                            "VALUES (" + userIndex + ", " + time + ", " + date + ")";
+                        query = "UPDATE salary SET salary.holidayPay = '" + time * 15000 + "' " +
+                            "WHERE salary.index = " + ID + " AND salary.date = '" + date + "'";
                         DBConnetion.getInstance().Update(query);
                         MessageBox.Show("휴일근무 ( " + time + " ) 시간 등록이 완료되었습니다.", "신청 확인");
                         break;
                 }
             }
-
-            if (check.Equals(1)) // 기존에 초과근무 했던 기록이 존재할 때
+            if (check.Equals(0))
             {
-                switch (select)
-                {
-                    case 0: // 연장근무
-                        query = "UPDATE ";
-                        DBConnetion.getInstance().Update(query);
-                        MessageBox.Show("연장근무 ( " + time + " ) 시간 등록이 완료되었습니다.", "신청 확인");
-                        break;
-                    case 1: // 야간근무
-                        query = "UPDATE ";
-                        DBConnetion.getInstance().Update(query);
-                        MessageBox.Show("야간근무 ( " + time + " ) 시간 등록이 완료되었습니다.", "신청 확인");
-                        break;
-                    case 2: // 휴일근무
-                        query = "UPDATE ";
-                        DBConnetion.getInstance().Update(query);
-                        MessageBox.Show("휴일근무 ( " + time + " ) 시간 등록이 완료되었습니다.", "신청 확인");
-                        break;
-                }
+                MessageBox.Show("먼저 출근버튼을 눌러주십시오.");
             }
         }
 
