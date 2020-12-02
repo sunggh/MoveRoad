4ceb32dd624ab385631fcab6b8b872ed6f3bb1be
이동길 <oio3215@naver.com>
2020-12-02 16:36:47 +0900

    111

--- MOVEROAD/depart_add_event.cs
+++ MOVEROAD/depart_add_event.cs
@@ -43,34 +43,43 @@ namespace MOVEROAD
             string get_index_query = "select count(`id`) as `id` from department";
             string count_id = DBConnetion.getInstance().get_department_countid(get_index_query);
 
+            //예외처리
+            if (depart_name.Equals("") || depart_head.Equals(""))
+            {
+                MessageBox.Show("내용을 입력해주세요.");
+            }
+            else
+            {
+                //그 사람의 인덱스로 검색하기
+                string add_query = "insert into department values ('" + count_id + "','" + depart_name + "'," + user.index + ",'" + depart_memo + "')";
+                DBConnetion.getInstance().Insert(add_query);
 
-            //그 사람의 인덱스로 검색하기
-            string add_query = "insert into department values ('"+count_id+"','"+depart_name+"',"+user.index+",'"+depart_memo+"')";
-            DBConnetion.getInstance().Insert(add_query);
+                //user.depart하면 부서 인덱스 쓸수있음.
 
-            //user.depart하면 부서 인덱스 쓸수있음.
+                string id_search = "select id from department where name = '" + depart_name + "'";
 
-            string id_search = "select id from department where name = '" + depart_name + "'";
+                int department_id = (int)DBConnetion.getInstance().Select(id_search, 20);
+                //id를 찾았음.
 
-            int department_id = (int)DBConnetion.getInstance().Select(id_search, 20);
-            //id를 찾았음.
+                //그 사람의 직급도 변경하고 원 소속의 부서를 새로 생성된 부서로 변경해야함
+                //새로 생성된 부서를 사용하기 위해서는 새로 생성된 부서의 id를 찾아야함.
 
-            //그 사람의 직급도 변경하고 원 소속의 부서를 새로 생성된 부서로 변경해야함
-            //새로 생성된 부서를 사용하기 위해서는 새로 생성된 부서의 id를 찾아야함.
+                string update_query = "update user set `depart` = '" + department_id + "', `grade` = 1 where `index` = " + user.index;
+                DBConnetion.getInstance().Update(update_query);
 
-            string update_query = "update user set `depart` = '"+department_id+"', `grade` = 1 where `index` = " + user.index;
-            DBConnetion.getInstance().Update(update_query);
+
+                //메인에 객체형태로 department 넣어주기 위함.
+
+
+                mf.departments.Add(new DepartmentInfo(department_id, depart_name, user.index, depart_memo));
+
+                MessageBox.Show("정상적으로 등록되었습니다.");
+
+                this.DialogResult = DialogResult.OK;
+                this.Dispose();
+            }
+
             
-
-            //메인에 객체형태로 department 넣어주기 위함.
-            
-
-            mf.departments.Add(new DepartmentInfo(department_id, depart_name, user.index , depart_memo));
-
-            MessageBox.Show("정상적으로 등록되었습니다.");
-
-            this.DialogResult = DialogResult.OK;
-            this.Dispose();
         }
 
         private void button1_Click(object sender, EventArgs e)
