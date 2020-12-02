c0f1c16741ad445ec332336012304457318d6fe4
5359998@stu.kmu.ac.kr <5359998@stu.kmu.ac.kr>
2020-12-02 17:31:23 +0900

    asdas

--- MOVEROAD/MainForm.cs
+++ MOVEROAD/MainForm.cs
@@ -86,6 +86,7 @@ namespace MOVEROAD
         public int room_id;
         private void chathandler(string message)
         {
+            if (message == "") return;
             string[] str = message.Split(new string[] { "|" }, StringSplitOptions.None);
             int opcode = int.Parse(str[0]);
             
