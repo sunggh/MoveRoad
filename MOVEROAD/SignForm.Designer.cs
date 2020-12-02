1b2c7831e2b0db6e0079aa077013439e47a0154f
sagongjieun <gonii0821@naver.com>
2020-12-02 16:22:05 +0900

    ㅁㄴㅇㄹ

--- MOVEROAD/SignForm.Designer.cs
+++ MOVEROAD/SignForm.Designer.cs
@@ -447,6 +447,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(71)))), ((int)(((byte)(117)))));
             this.ClientSize = new System.Drawing.Size(851, 515);
             this.Controls.Add(this.tabControlSign);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
