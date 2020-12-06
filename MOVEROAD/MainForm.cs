fc2d744fe4116609469a7f6def95e5cee01af54f
동길 이 <oio3215@naver.com>
2020-12-06 21:02:02 +0900

    123

--- MOVEROAD/MainForm.cs
+++ MOVEROAD/MainForm.cs
@@ -63,7 +63,7 @@ namespace MOVEROAD
             try 
             {
                 
-                clientSocket.Connect("121.150.220.47", 80);//211.229.51.245
+                clientSocket.Connect("121.150.220.47", 80);
                 stream = clientSocket.GetStream();
                 message = "1|"+me.index;
                 byte[] buffer = Encoding.Unicode.GetBytes(message);
